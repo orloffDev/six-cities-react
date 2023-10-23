fad40c87ccf863fa48df4fd9443cfe0227936b34
igl <3wmaster@list.ru>
2023-10-24 00:03:02 +0300

    module2-task1 fixed

--- src/pages/login-screen/login-screen.tsx
+++ src/pages/login-screen/login-screen.tsx
@@ -12,7 +12,7 @@ function LoginScreen(): JSX.Element {
             </div>
             <div className="login__input-wrapper form__input-wrapper">
               <label className="visually-hidden">Password</label>
-              <input className="login__input form__input" type="password" name="password" placeholder="Password" required="required" />
+              <input className="login__input form__input" type="password" name="password" placeholder="Password" required />
             </div>
             <button className="login__submit form__submit button" type="submit">Sign in</button>
           </form>
