27ca60db2227ec9cff891fc15fe8c3303e0c918e
igl <3wmaster@list.ru>
2023-11-06 23:00:49 +0300

    add logo component

---
+++ src/components/logo/logo.tsx
@@ -0,0 +1,17 @@
+import {Link} from 'react-router-dom';
+
+type LogoProps = {
+  pageName: string;
+}
+
+function Logo({pageName}): JSX.Element {
+  const cls = 'header__logo-link' +  (pageName === 'welcome' ? 'header__logo-link--active' : '');
+
+  return (
+    <Link className={cls} to="/">
+      <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
+    </Link>
+  );
+}
+
+export default Logo;
--- src/pages/welcome-screen/welcome-screen.tsx
+++ src/pages/welcome-screen/welcome-screen.tsx
@@ -1,3 +1,4 @@
+import Logo from '../../components/logo/logo';
 import PlaceCard from '../../components/place-card/place-card';
 
 type WelcomeScreenProps = {
@@ -12,9 +13,7 @@ function WelcomeScreen({placesFound}: WelcomeScreenProps): JSX.Element {
         <div className="container">
           <div className="header__wrapper">
             <div className="header__left">
-              <a className="header__logo-link header__logo-link--active">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </a>
+              {<Logo pageName="welcome"></Logo>}
             </div>
             <nav className="header__nav">
               <ul className="header__nav-list">
