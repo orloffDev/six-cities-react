742388a6b80d7b41b66a4dd7d8693a7cc1fa189f
igl <3wmaster@list.ru>
2023-11-06 20:44:31 +0300

    add router + not-fount-screen

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,12 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import {AppRoute} from '../../const';
+
+
+import FavoritesScreen from '../../pages/favorites-screen/favorites-screen';
+import LoginScreen from '../../pages/login-screen/login-screen';
+import OfferScreen from '../../pages/offer-screen/offer-screen';
 import WelcomeScreen from '../../pages/welcome-screen/welcome-screen';
+import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
 
 type AppProps = {
   placesFound: number;
@@ -6,7 +14,30 @@ type AppProps = {
 
 function App({placesFound}: AppProps): JSX.Element {
   return (
-    <WelcomeScreen placesFound={placesFound} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<WelcomeScreen placesFound={placesFound} />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={<FavoritesScreen />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginScreen />}
+        />
+        <Route
+          path={AppRoute.Offer}
+          element={<OfferScreen />}
+        />
+        <Route
+          path="*"
+          element={<NotFoundScreen />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/const.ts
@@ -0,0 +1,13 @@
+export enum AppRoute {
+  Login = '/login',
+  Offer = '/offer',
+  Favorites = '/favorites',
+  Root = '/',
+}
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
+
