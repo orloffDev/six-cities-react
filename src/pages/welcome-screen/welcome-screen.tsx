d77bd2af74b0d64176c14ba70b9ba9da97f7f5f4
igl <3wmaster@list.ru>
2023-11-08 19:34:46 +0300

    fixed ESLint error Error:   85:1   error  Expected indentation of 18 spaces but found 16        indent Error:   86:1   error  Expected indentation of 20 spaces but found 18        indent Error:   87:1   error  Expected indentation of 18 spaces but found 16        indent Error:   88:1   error  Expected indentation of 16 spaces but found 14        indent Error:   88:15  error  Expected closing tag to match indentation of opening  react/jsx-closing-tag-location

--- src/pages/welcome-screen/welcome-screen.tsx
+++ src/pages/welcome-screen/welcome-screen.tsx
@@ -81,11 +81,11 @@ function WelcomeScreen({placesFound}: WelcomeScreenProps): JSX.Element {
               <form className="places__sorting" action="#" method="get">
                 <span className="places__sorting-caption">Sort by</span>
                 <span className="places__sorting-type" tabIndex={0}>
-                Popular
-                <svg className="places__sorting-arrow" width="7" height="4">
-                  <use xlinkHref="#icon-arrow-select"></use>
-                </svg>
-              </span>
+                  Popular
+                  <svg className="places__sorting-arrow" width="7" height="4">
+                    <use xlinkHref="#icon-arrow-select"></use>
+                  </svg>
+                </span>
                 <ul className="places__options places__options--custom places__options--opened">
                   <li className="places__option places__option--active" tabIndex={0}>Popular</li>
                   <li className="places__option" tabIndex={0}>Price: low to high</li>
