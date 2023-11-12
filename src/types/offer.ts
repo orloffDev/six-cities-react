3368c0721abe96a3c40056fd82a7db8938dd1469
igl <3wmaster@list.ru>
2023-11-12 16:11:59 +0300

    4.1 add mocks offers

---
+++ src/types/offer.ts
@@ -0,0 +1,15 @@
+import {City} from './city';
+import {Location} from './location';
+
+export type Offer = {
+  id: string
+  title: string
+  type: string
+  price: number
+  city: City
+  location: Location,
+  isFavorite: boolean
+  isPremium: boolean
+  rating: number
+  previewImage: string
+}
