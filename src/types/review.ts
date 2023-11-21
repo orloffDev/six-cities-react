4d383ec42e6270529415ecfb3e91c2e063dffb93
igl <3wmaster@list.ru>
2023-11-21 22:29:31 +0300

    build bug fixed

--- src/types/review.ts
+++ src/types/review.ts
@@ -1,11 +1,11 @@
 import {User} from './user';
 
 export type Review = {
-  id: string
-  date: string
-  user: User
-  comment: string
-  rating: number
+  id: string;
+  date: string;
+  user: User;
+  comment: string;
+  rating: number;
 }
 
 
