7896d190f29da5c764545968e3ca6a1aa14c98ee
igl <3wmaster@list.ru>
2023-11-21 22:28:06 +0300

    build bug fixed

--- src/mocks/reviews-data.ts
+++ src/mocks/reviews-data.ts
@@ -2,14 +2,14 @@ import {Review} from '../types/review';
 
 export const reviewsData:Review[] = [
   {
-    "id": "b67ddfd5-b953-4a30-8c8d-bd083cd6b62a",
-    "date": "2019-05-08T14:13:56.569Z",
-    "user": {
-      "name": "Oliver Conner",
-      "avatarUrl": "https://url-to-image/image.png",
-      "isPro": false
+    'id': 'b67ddfd5-b953-4a30-8c8d-bd083cd6b62a',
+    'date': '2019-05-08T14:13:56.569Z',
+    'user': {
+      'name': 'Oliver Conner',
+      'avatarUrl': 'https://url-to-image/image.png',
+      'isPro': false
     },
-    "comment": "A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.",
-    "rating": 4
+    'comment': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    'rating': 4
   }
 ] ;
