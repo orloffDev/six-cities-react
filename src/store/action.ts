826db22d0784a91f15d603b6ab4eb4c5d64f833a
igl <3wmaster@list.ru>
2023-11-25 19:32:48 +0300

    build bug fixed

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,13 +1,5 @@
 import { createAction } from '@reduxjs/toolkit';
 
-export const setActiveCityName = createAction('city/change', (value) => {
-  return {
-    payload: value
-  };
-});
+export const setActiveCityName = createAction('city/change');
 
-export const setOffers = createAction('offers/change', (value) => {
-  return {
-    payload: value
-  };
-});
+export const setOffers = createAction('offers/change');
