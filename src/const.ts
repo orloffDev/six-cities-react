07a4bd212c7051d54b8c9f8a87d5f61fddc00013
igl <3wmaster@list.ru>
2023-11-25 20:21:17 +0300

    build bug fixed

--- src/const.ts
+++ src/const.ts
@@ -22,7 +22,7 @@ export enum CitiesList {
   Dusseldorf = 'Dusseldorf'
 }
 
-export const CITY_DEFAULT_NAME = CitiesList.Paris;
+export const CITY_DEFAULT_NAME = 'Paris';
 
 export const URL_MARKER_DEFAULT = 'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg';
 
