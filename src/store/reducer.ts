b5f2dc1dcd91ac227d209535531edbd51ffef67a
igl <3wmaster@list.ru>
2023-11-26 13:00:57 +0300

    bug build fixed

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -2,7 +2,7 @@ import {createReducer} from '@reduxjs/toolkit';
 import {setActiveCityName, setOffers} from './action';
 //const
 import {CITY_DEFAULT_NAME } from '../const';
-import {offers} from "../mocks/offers";
+import {offers} from '../mocks/offers';
 
 const initialState = {
   activeCityName: CITY_DEFAULT_NAME,
