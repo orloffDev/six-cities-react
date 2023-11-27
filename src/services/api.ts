dd053f8b43e47f55c06ce370a691f211fb4354f6
igl <3wmaster@list.ru>
2023-11-27 13:52:40 +0300

    7.4 add token support build bug fixed

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-import axios, {AxiosInstance, AxiosRequestConfig} from 'axios';
+import axios, {AxiosInstance, AxiosRequestConfig, InternalAxiosRequestConfig} from 'axios';
 import {BACKEND_URL, REQUEST_TIMEOUT} from '../const';
 import {getToken} from './token';
 
@@ -9,7 +9,7 @@ export const createAPI = (): AxiosInstance => {
   });
 
   api.interceptors.request.use(
-    (config: AxiosRequestConfig) => {
+    (config: InternalAxiosRequestConfig) => {
       const token = getToken();
 
       if (token && config.headers) {
