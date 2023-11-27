75e5c9629c4abf89daa928333ba7f900f45a73c1
igl <3wmaster@list.ru>
2023-11-28 01:17:44 +0300

    build bug fixed

--- src/services/api.ts
+++ src/services/api.ts
@@ -15,7 +15,7 @@ const StatusCodeMapping: Record<number, boolean> = {
   [StatusCodes.NOT_FOUND]: true
 };
 
-const shouldDisplayError = (response: AxiosResponse, AxiosResponse, AxiosError) => !!StatusCodeMapping[response.status];
+const shouldDisplayError = (response: AxiosResponse) => !!StatusCodeMapping[response.status];
 
 export const createAPI = (): AxiosInstance => {
   const api = axios.create({
