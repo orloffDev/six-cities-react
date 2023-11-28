15bbef4c40f2c4b3234b70bef8de0586e6e32b2e
igl <3wmaster@list.ru>
2023-11-29 00:51:06 +0300

    build bug fixed

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,4 @@
-import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 import {HelmetProvider} from 'react-helmet-async';
 import {useAppSelector} from '../../hooks/use-app-selector';
 import {AppRoute, AuthorizationStatus} from '../../const';
