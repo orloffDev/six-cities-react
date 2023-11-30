056a6d5b250a3fc4568ba913582a04b8ca10cd08
igl <3wmaster@list.ru>
2023-11-30 13:35:59 +0300

    add dynamic data

--- src/pages/offer-screen/offer-screen.tsx
+++ src/pages/offer-screen/offer-screen.tsx
@@ -145,25 +145,18 @@ function OfferScreen(): JSX.Element {
                 <div className="offer__host">
                   <h2 className="offer__host-title">Meet the host</h2>
                   <div className="offer__host-user user">
-                    <div className="offer__avatar-wrapper offer__avatar-wrapper--pro user__avatar-wrapper">
-                      <img className="offer__avatar user__avatar" src="img/avatar-angelina.jpg" width="74" height="74" alt="Host avatar" />
+                    <div className={`offer__avatar-wrapper user__avatar-wrapper ${offer.host.isPro ? 'offer__avatar-wrapper--pro' : ''}`}>
+                      <img className="offer__avatar user__avatar" src={offer.host.avatarUrl} width="74" height="74" alt="Host avatar" />
                     </div>
                     <span className="offer__user-name">
-                      Angelina
+                      {offer.host.name}
                     </span>
-                    <span className="offer__user-status">
+                    {offer.host.isPro && <span className="offer__user-status">
                       Pro
-                    </span>
+                    </span>}
                   </div>
                   <div className="offer__description">
-                    <p className="offer__text">
-                      A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The
-                      building is green and from 18th century.
-                    </p>
-                    <p className="offer__text">
-                      An independent House, strategically located between Rembrand Square and National Opera, but where the
-                      bustle of the city comes to rest in this alley flowery and colorful.
-                    </p>
+                    <p className="offer__text">{offer.description}</p>
                   </div>
                 </div>
                 <section className="offer__reviews reviews">
