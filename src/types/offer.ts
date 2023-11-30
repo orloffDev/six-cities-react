967600b8e345f18ac85332b4799642d74e967734
igl <3wmaster@list.ru>
2023-11-30 10:42:54 +0300

    update offer type

--- src/types/offer.ts
+++ src/types/offer.ts
@@ -12,6 +12,17 @@ export type Offer = {
   isPremium: boolean;
   rating: number;
   previewImage: string;
+
+  description: string;
+  bedrooms: number;
+  goods: [string];
+  host: {
+    name: string;
+    avatarUrl: string;
+    isPro: boolean;
+  };
+  images: [string];
+  maxAdults: number;
 }
 
 export type OfferHandleEnter= (offer: Offer) => void
