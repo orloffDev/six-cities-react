aac5bb5bf08dd97386ebc874d8a094d97a25f44f
igl <3wmaster@list.ru>
2023-12-01 00:26:13 +0300

    bug build fixed

--- src/components/header/fav-count.tsx
+++ src/components/header/fav-count.tsx
@@ -1,8 +1,5 @@
-import {useAppSelector} from "../../hooks/use-app-selector";
 
 function FavCount(): JSX.Element {
-  alert(111);
-  const userFavCount = useAppSelector((state) => state.userFavCount);
   return (
     <span className="header__favorite-count">3</span>
   );
