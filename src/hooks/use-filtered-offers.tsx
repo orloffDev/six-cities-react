20227310c29c6e3ce7458373833ceae7699fd59d
igl <3wmaster@list.ru>
2023-12-03 17:45:26 +0300

    build bug fixed

--- src/hooks/use-filtered-offers.tsx
+++ src/hooks/use-filtered-offers.tsx
@@ -2,26 +2,24 @@ import {Offer} from '../types/offer';
 import {CityName} from '../types/city-name';
 import {SortingOption} from '../const';
 
-export const useFilteredOffers = function (offers: Offer[], cityName: CityName, optionValue?: string): Offer[] {
-  const byCity: Offer[] = offers.filter((offer) => offer.city.name === cityName);
-  const byField = (byCity): Offer[] => {
-    if(!optionValue){
-      return byCity;
-    }
-    switch (optionValue) {
-      case SortingOption.Popular:
-        return byCity;
-      case SortingOption.PriceLowToHigh:
-        return byCity.sort((a: Offer, b: Offer) => a.price - b.price);
-      case SortingOption.PriceHighToLow:
-        return byCity.sort((a: Offer, b: Offer) => b.price - a.price);
-      case SortingOption.TopRatedFirst:
-        return byCity.sort((a: Offer, b: Offer) => b.rating - a.rating);
-      default:
-        return byCity;
-    }
-  };
 
-  return !optionValue ? byCity : byField(byCity);
+
+export const useFilteredOffers = function (offers: Offer[], cityName: CityName, optionValue?: string): Offer[] {
+  const newFilteredOffer: Offer[] = offers.filter((offer) => offer.city.name === cityName);
+  if(optionValue){
+    switch (optionValue) {
+      case SortingOption.PriceLowToHigh:
+        newFilteredOffer.sort((a: Offer, b: Offer) => a.price - b.price);
+        break;
+      case SortingOption.PriceHighToLow:
+        newFilteredOffer.sort((a: Offer, b: Offer) => b.price - a.price);
+        break;
+      case SortingOption.TopRatedFirst:
+        newFilteredOffer.sort((a: Offer, b: Offer) => b.rating - a.rating);
+        break;
+    }
+  }
+
+  return newFilteredOffer;
 }
 
