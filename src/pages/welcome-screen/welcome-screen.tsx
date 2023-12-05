883e4400da336a1be75c6a6e3f413af8ab564c56
igl <3wmaster@list.ru>
2023-12-05 15:56:36 +0300

    add main page index

--- src/pages/welcome-screen/welcome-screen.tsx
+++ src/pages/welcome-screen/welcome-screen.tsx
@@ -12,6 +12,8 @@ import {Helmet} from 'react-helmet-async';
 import SortingForm from '../../components/sorting-form/sorting-form';
 import {useFilteredOffers} from '../../hooks/use-filtered-offers';
 import {SORTING_DEFAULT_OPTION} from '../../const';
+import classNames from 'classnames';
+
 
 function WelcomeScreen(): JSX.Element {
   const offers = useAppSelector((state) => state.offers);
@@ -21,6 +23,7 @@ function WelcomeScreen(): JSX.Element {
   const [activeOptionValue, setActiveOptionValue] = useState<string>(SORTING_DEFAULT_OPTION);
   const filteredOffersData: Offer[] = useFilteredOffers(offers, activeCityName, activeOptionValue);
   const placesFound: number = filteredOffersData.length;
+  const mainClasses = classNames(['page__main page__main--index', !placesFound && 'page__main--index-empty']);
 
   const onChangeHoverPlaceList = function(offer: Offer){
     const newSelectedPoint: SelectedPoint = {
@@ -47,11 +50,12 @@ function WelcomeScreen(): JSX.Element {
 
       <div className="page page--gray page--main">
         <Header nav />
-        <main className="page__main page__main--index">
+        <main className={mainClasses}>
           <h1 className="visually-hidden">Cities</h1>
 
           <Tabs />
 
+          {placesFound !== 0 &&
           <div className="cities">
             <div className="cities__places-container container">
               <section className="cities__places places">
@@ -72,6 +76,23 @@ function WelcomeScreen(): JSX.Element {
               </div>
             </div>
           </div>
+          }
+
+          {placesFound === 0 &&
+          <div className="cities">
+            <div className="cities__places-container cities__places-container--empty container">
+              <section className="cities__no-places">
+                <div className="cities__status-wrapper tabs__content">
+                  <b className="cities__status">No places to stay available</b>
+                  <p className="cities__status-description">We could not find any property available at the moment in
+                    Dusseldorf</p>
+                </div>
+              </section>
+              <div className="cities__right-section"></div>
+            </div>
+          </div>
+          }
+
         </main>
       </div>
     </>
