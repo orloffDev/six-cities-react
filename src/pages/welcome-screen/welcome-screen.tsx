36f01f87585606c3251a882567d610a5b5594491
igl <3wmaster@list.ru>
2023-12-05 16:05:57 +0300

    build bug fixed

--- src/pages/welcome-screen/welcome-screen.tsx
+++ src/pages/welcome-screen/welcome-screen.tsx
@@ -83,8 +83,7 @@ function WelcomeScreen(): JSX.Element {
               <section className="cities__no-places">
                 <div className="cities__status-wrapper tabs__content">
                   <b className="cities__status">No places to stay available</b>
-                  <p className="cities__status-description">We could not find any property available at the moment in
-                    Dusseldorf</p>
+                  <p className="cities__status-description">We could not find any property available at the moment in Dusseldorf</p>
                 </div>
               </section>
               <div className="cities__right-section"></div>
