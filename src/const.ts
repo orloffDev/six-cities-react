346b97c736e6571f7db7fe6fb0611e9ab9ac9a08
igl <3wmaster@list.ru>
2023-12-07 22:36:46 +0300

    build bug fixed

--- src/const.ts
+++ src/const.ts
@@ -4,7 +4,8 @@ export enum AppRoute {
   Main = '/',
   Login = '/login',
   Favorites = '/favorites',
-  Offer = '/offer/:id'
+  Offer = '/offer/:id',
+
 }
 
 export enum AuthorizationStatus {
--- src/types/InitialState.ts
+++ src/types/InitialState.ts
@@ -2,7 +2,6 @@ import {CityName} from './city-name';
 import {Offer} from './offer';
 import {AuthorizationStatus} from '../const';
 import {UserData} from './user-data';
-import {OfferItem} from "./offer-item";
 
 export type InitialState = {
   activeCityName: CityName;
