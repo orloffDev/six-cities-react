edf5c9db32494b71bd0d9c17cdc8da1d069085b0
igl <3wmaster@list.ru>
2023-12-09 11:47:45 +0300

    bug fixed city data

--- src/store/city-data/city-data.ts
+++ src/store/city-data/city-data.ts
@@ -2,7 +2,6 @@ import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 import {NameSpace, CITY_DEFAULT_NAME} from '../../const';
 import {CityData} from '../../types/state';
 import {CityName} from "../../types/city-name";
-import {offersData} from "../offers-data/offers-data";
 
 const initialState: CityData = {
   activeCityName: CITY_DEFAULT_NAME
@@ -12,9 +11,8 @@ export const cityData = createSlice({
   name: NameSpace.City,
   initialState,
   reducers: {
-    setActiveCityName: (state, action: PayloadAction<{activeCityName: CityName}>) => {
-      const {activeCityName} = action.payload;
-      state.activeCityName = activeCityName;
+    setActiveCityName: (state, action: PayloadAction<CityName>) => {
+      state.activeCityName = action.payload;
     }
   }
 });
