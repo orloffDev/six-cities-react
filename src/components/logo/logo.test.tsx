289b1576ddde84497ab74d4a4c8174971b617073
igl <3wmaster@list.ru>
2023-12-09 21:10:05 +0300

    logo test

--- src/components/logo/logo.test.tsx
+++ src/components/logo/logo.test.tsx
@@ -1,12 +1,10 @@
 import { render, screen} from '@testing-library/react';
 import Logo from "./logo";
-import {configureMockStore} from "@jedmao/redux-mock-store";
-import {makeFakeStore} from "../../utils/mocks";
 import {createMemoryHistory} from "history";
 import {AppRoute} from "../../const";
 import HistoryRouter from "../history-router/history-router";
 
-const mockStore = configureMockStore();
+
 
 const history = createMemoryHistory();
 
