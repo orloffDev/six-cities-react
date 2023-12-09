b259685813eeee0f75f8e5d738a38465897a5852
igl <3wmaster@list.ru>
2023-12-09 19:17:55 +0300

    bug fixed

--- src/utils/mocks.ts
+++ src/utils/mocks.ts
@@ -5,7 +5,7 @@ import {Location} from '../types/location';
 import {User} from '../types/user';
 import {State } from '../types/state';
 import {UserData} from '../types/user-data';
-import {AuthorizationStatus, CitiesList} from '../const';
+import {AuthorizationStatus, CitiesList, CITY_DEFAULT_NAME} from '../const';
 
 export const makeFakeUserData = (): UserData => ({
   avatarUrl: internet.avatar(),
@@ -60,7 +60,7 @@ export const makeFakeOffer = (): Offer => ({
 
 export const makeFakeStore = (newStateData?: Partial<State>): State => ({
   USER: { userData: makeFakeUserData(), authorizationStatus: AuthorizationStatus.Auth},
-  CITY: { city: CitiesList[0] },
+  CITY: { city: CITY_DEFAULT_NAME},
   OFFERS: {
     offers: [makeFakeOffer(), makeFakeOffer()],
     isOffersDataLoading: false
