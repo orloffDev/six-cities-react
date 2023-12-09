c80acbab53d37d7896f7a9729172fc2b03bbe801
igl <3wmaster@list.ru>
2023-12-09 21:00:45 +0300

    logo test

--- src/components/logo/logo.test.tsx
+++ src/components/logo/logo.test.tsx
@@ -1,17 +1,29 @@
 import { render, screen} from '@testing-library/react';
-import {createMemoryHistory } from 'history';
-import {Provider} from 'react-redux';
-import React from 'react';
+import {Provider} from "react-redux";
+import Logo from "./logo";
+import {configureMockStore} from "@jedmao/redux-mock-store";
+import {makeFakeStore} from "../../utils/mocks";
+import {createMemoryHistory} from "history";
+import App from "../app/app";
+import {AppRoute} from "../../const";
+
+const mockStore = configureMockStore();
+const store = mockStore(makeFakeStore());
 
 const history = createMemoryHistory();
 
+const fakeApp = (
+  <Provider store={store}>
+    <App>
+      <Logo />
+    </App>
+  </Provider>
+);
+
 describe('Component: Logo', () => {
   it('should render correctly', () => {
-    render(
-      <HistoryRouter history={history}>
-        <Logo />
-      </HistoryRouter>);
-
+    history.push(AppRoute.Main);
+    render(fakeApp);
     expect(screen.getByAltText('6 cities logo')).toBeInTheDocument();
   });
 });
