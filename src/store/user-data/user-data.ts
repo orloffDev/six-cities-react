024aadecf64c85c59d04cd16e00845224d3652ee
igl <3wmaster@list.ru>
2023-12-09 11:32:18 +0300

    del unused

--- src/store/user-data/user-data.ts
+++ src/store/user-data/user-data.ts
@@ -1,9 +1,7 @@
 import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 import {NameSpace, AuthorizationStatus} from '../../const';
 import {UserProcess} from '../../types/state';
-import {checkAuthAction, loginAction, logoutAction} from '../api-actions';
 import {UserData} from "../../types/user-data";
-import {Offer} from "../../types/offer";
 
 const initialState: UserProcess = {
   authorizationStatus: AuthorizationStatus.Unknown,
