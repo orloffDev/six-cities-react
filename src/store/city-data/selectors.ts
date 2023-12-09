5e47f2e9aad0d082e65056eafa5f56724410983d
igl <3wmaster@list.ru>
2023-12-09 14:52:57 +0300

    build bug fixed

--- src/store/city-data/selectors.ts
+++ src/store/city-data/selectors.ts
@@ -1,5 +1,5 @@
 import {NameSpace} from '../../const';
 import {State} from '../../types/state';
-import {CityName} from "../../types/city-name";
+import {CityName} from '../../types/city-name';
 
 export const getActiveCityName = (state: State): CityName => state[NameSpace.City].activeCityName;
