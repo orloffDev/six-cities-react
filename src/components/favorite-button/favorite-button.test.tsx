da1ffe01cad87b93e23eb9f844f51a9e1a44f078
igl <3wmaster@list.ru>
2023-12-10 18:38:00 +0300

    add favorite-button.test.tsx

---
+++ src/components/favorite-button/favorite-button.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react';
+import { withStore, withHistory } from '../../utils/mock-component';
+import { makeFakeStore, makeFakeOffer } from '../../utils/mocks';
+import FavoriteButton from "./favorite-button";
+
+describe('Component: FavoriteButton', () => {
+  it('should render correctly', () => {
+    const fakeOffer = makeFakeOffer();
+    const fakeStore = makeFakeStore({});
+    const { withStoreComponent } = withStore(withHistory(
+      <FavoriteButton
+        offer={fakeOffer}
+        parent="place-card"
+        width={18}
+        height={19}
+      />), fakeStore);
+
+    render(withStoreComponent);
+
+    expect(screen.getByText('To bookmarks')).toBeInTheDocument();
+  });
+});
