bf2c52d828a74cda599d88db8c7346f897a66b5f
igl <3wmaster@list.ru>
2023-12-10 21:39:19 +0300

    build bug fixed

--- src/components/reviews-item/reviews-item.test.tsx
+++ src/components/reviews-item/reviews-item.test.tsx
@@ -1,7 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { withStore, withHistory } from '../../utils/mock-component';
-import {makeFakeStore, makeFakeOfferId, makeFakeReview} from '../../utils/mocks';
-import ReviewsForm from './reviews-form';
+import {makeFakeStore, makeFakeReview} from '../../utils/mocks';
 import ReviewsItem from "./reviews-item";
 
 describe('Component: ReviewsItem', () => {
